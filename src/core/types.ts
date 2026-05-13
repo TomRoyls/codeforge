2124b18eb9317526f17ab75143e6b3bb023f71eb
TomRoyls <tom.royls@gmail.com>
2026-05-13 19:47:43 +0200

    fix: remove agent debris files

--- src/core/types.ts
+++
@@ -1,1 +0,0 @@
-export type CompareFn<T> = (a: T, b: T) => number
--- test/quick_test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import { PatienceSort3 } from '../src/core/patience-sort-3/index'
-
-describe('Quick Test', () => {
-  it('should sort two elements', () => {
-    const sorter = new PatienceSort3<number>()
-    expect(sorter.sort([2, 1])).toEqual([1, 2])
-  })
-})
