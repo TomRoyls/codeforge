01c5d7d64840cc26be0000b373a29f268cc81802
TomRoyls <tom.royls@gmail.com>
2026-06-09 10:49:42 +0200

    feat: add Symbol.iterator to multimap-3
    Total modules with iterators: ~313.

--- src/core/multimap-3/index.ts
+++ src/core/multimap-3/index.ts
@@ -85,4 +85,17 @@ export class MultiMap3<K, V> {
     const values = this.map.get(key);
     return values ? values.length : 0;
   }
+
+  [Symbol.iterator](): Iterator<[K, V[]]> {
+    const items = this.entries();
+    let index = 0;
+    return {
+      next(): IteratorResult<[K, V[]]> {
+        if (index < items.length) {
+          return { value: items[index++]!, done: false };
+        }
+        return { value: undefined as unknown as [K, V[]], done: true };
+      },
+    };
+  }
 }
